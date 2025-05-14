fd55a4fc195b844d51e3e31f11db899dd89992bf
Pruthvi <23f1002103@ds.study.iitm.ac.in>
2025-05-14 07:56:46 +0530

    Initial commit

---
+++ api/index.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, 'q-vercel-python.json')
+with open(file_path, 'r') as f:
+    student_data = json.load(f)
+
+
+@app.route("/api/students/marks", methods=["GET"])
+def get_student_marks():
+  names = request.args.getlist("name")
+
+  marks=[]
+  for name in names:
+    mark = next((item['mark'] for item in student_data if item['name'] == name), None)
+    marks.append(mark)
+
+    return jsonify({"marks": marks})
+  
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
