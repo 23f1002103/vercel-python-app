3d2e79bf09866dc4ecd6f65d7cca94c290bbd179
23f1002103 <23f1002103@ds.study.iitm.ac.in>
2025-05-14 08:32:27 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,26 +6,19 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, 'q-vercel-python.json')
 with open(file_path, 'r') as f:
     student_data = json.load(f)
 
-
-@app.route("/api/students/marks", methods=["GET"])
+@app.route("/api", methods=["GET"])
 def get_student_marks():
-  names = request.args.getlist("name")
-
-  marks=[]
-  for name in names:
-    mark = next((item['mark'] for item in student_data if item['name'] == name), None)
-    marks.append(mark)
-
+    names = request.args.getlist("name")
+    marks = []
+    for name in names:
+        mark = next((item['mark'] for item in student_data if item['name'] == name), None)
+        marks.append(mark)
     return jsonify({"marks": marks})
   
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
